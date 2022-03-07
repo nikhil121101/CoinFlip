530950fc8a63531bc8df91ae3305d7e870300cc8
Nikhil Mishra <nikhilmishra1211@gmail.com>
2022-03-07 19:40:28 +0530

    Create CoinFlip.sol

---
+++ CoinFlip.sol
@@ -0,0 +1,61 @@
+pragma solidity >=0.7.0 <0.9.0;
+
+
+contract CoinFlip {
+
+    struct Account {
+        bool created;
+        int balance;
+    }
+
+    mapping(address => Account) public accounts; 
+
+    function createAccountIfNeeded(address user) private{
+        if(!accounts[user].created) {
+            accounts[user].created = true;
+            accounts[user].balance = 100;
+        }
+    }
+
+    mapping(address => bool) betStatus; // if a user has put a bet till now.
+
+    struct Bet {
+        address user;
+        int guess;
+        int amount;
+    }
+
+    Bet[] public currentBets;
+
+    function placeBet(int _amount, int _guess) public {
+        address _user = msg.sender;
+        createAccountIfNeeded(_user);
+        require(
+            accounts[_user].balance >= _amount,
+            "Insufficient balance."
+        );
+        require(
+            !betStatus[_user],
+            "Bet Already placed."
+        );
+        currentBets.push(Bet(_user, _guess, _amount));
+        accounts[_user].balance -= _amount;
+        betStatus[_user] = true;
+    }
+
+    function rewardBets() public {
+        int finalOutcome = 0;
+        for(uint i = 0; i < currentBets.length; i++) {
+            if(currentBets[i].guess == finalOutcome) {
+                // winner
+                accounts[currentBets[i].user].balance += 2*currentBets[i].amount;
+            }
+            else {
+                // looser
+            }
+            betStatus[currentBets[i].user] = false;
+        }
+        delete currentBets;
+    }
+
+}
